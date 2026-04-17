0b0947ac71b663ca5a535c32af4eef53b26cc94c
robles-9r-ay2526 <robles.db@obmontessori.edu.ph>
2026-04-17 13:27:34 +0800

    AA

---
+++ main.js
@@ -0,0 +1,9 @@
+function checkmydih(){
+    let password = "";
+
+do {        
+    password = prompt("Enter a password longer than 4 characters", "");
+} while (password.length < 5);
+
+window.alert("Entered password: ", password);
+}
